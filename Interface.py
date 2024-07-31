213ee251b1c53b568672bf2b6c34e6773c910272
LouisMHT <128934086+LouisMHT@users.noreply.github.com>
2024-07-31 09:45:29 +0200

    save

--- Interface.py
+++ Interface.py
@@ -4,14 +4,27 @@ from ttkthemes import ThemedTk
 from tkinter.font import Font
 from PIL import Image, ImageTk
 
+# Fonction pour basculer entre les frames
+def show_frame(frame):
+    frame.tkraise()
+
 # Crée la fenêtre principale avec le thème Adapta
 root = ThemedTk(theme="adapta")  # Appliquer le thème Adapta
 root.title("GRDF - DMOSControl")
-root.geometry("1280x720")
+root.geometry("854x480")
 root.resizable(False, False)
 icon = tk.PhotoImage(file="logo3.png")
 root.iconphoto(False, icon)
 root.configure(bg="white")
+
+# Crée deux frames
+frame1 = tk.Frame(root, bg="white")
+frame2 = tk.Frame(root, bg="white")
+
+# Utilisation de place pour les frames
+frame1.place(x=0, y=0, relwidth=1, relheight=1)
+frame2.place(x=0, y=0, relwidth=1, relheight=1)
+
 custom_font = Font(family="Calibri", size=16, weight="bold")
 custom_font2 = Font(family="Calibri", size=10, weight="bold", slant="italic")
 
@@ -22,18 +35,20 @@ image = image.resize((298, 148))
 photo = ImageTk.PhotoImage(image)
 
 # Ajouter l'image à la première frame
-image_label = tk.Label(image=photo, bg="white")
-image_label.place(x=491, y=150)
+image_label = tk.Label(frame1, image=photo, bg="white")
+image_label.place(x=278, y=75)
 
 # Ajouter un label et un bouton
-label = ttk.Label(root, text="Bienvenue dans l'application de prévalidation de DMOS !", font=custom_font, background="white")
-label.place(relx=0.5, anchor='center', y=330)
+label = ttk.Label(frame1, text="Bienvenue dans l'application de prévalidation de DMOS !", font=custom_font, background="white")
+label.place(relx=0.5, anchor='center', y=250)
 
-button = ttk.Button(root, text="Commencer")
-button.place(relx=0.5, anchor='center', y=400)
+button = ttk.Button(frame1, text="Commencer")
+button.place(relx=0.5, anchor='center', y=315)
 
-label2 = ttk.Label(root, text="Application en Bêta - Louis THIERRY", font=custom_font2, foreground="grey", background="white")
-label2.place(relx=0.5, anchor='center', y=700)
+label2 = ttk.Label(frame1, text="Application en Bêta - LT", font=custom_font2, foreground="grey", background="white")
+label2.place(relx=0.5, anchor='center', y=460)
+
+show_frame(frame1)
 
 # Démarrer la boucle principale
 root.mainloop()
